28b4af87a00030b7790677574f8a95d7bad0dfd8
Phan Anh Dung <phananhdung309@yahoo.com>
2013-06-03 19:32:38 +0200

    Add Html template and ignore generated output

--- literate/Build.fsx
+++ literate/Build.fsx
@@ -4,12 +4,23 @@
 open System.IO
 open FSharp.Literate
 
+let generateHtmlDoc() =
+  let source = __SOURCE_DIRECTORY__ + "./.."
+  let template = Path.Combine(source, "./templates/template-project.html")
+  let sources = Path.Combine(source, ".")
+  let output = Path.Combine(source, ".")
+  printfn "Generate fsharp-cheatsheet.html"
+  Literate.ProcessMarkdown
+      ( Path.Combine(source, "fsharp-cheatsheet.md"), template,
+        Path.Combine(output, "fsharp-cheatsheet.html"), OutputKind.Html,
+        includeSource = true)
+
 let generateLatexDoc() =
   let source = __SOURCE_DIRECTORY__ + "./.."
   let template = Path.Combine(source, "./templates/template-cheatsheet.tex")
   let sources = Path.Combine(source, ".")
   let output = Path.Combine(source, ".")
-
+  printfn "Generate fsharp-cheatsheet.tex"
   Literate.ProcessMarkdown
       ( Path.Combine(source, "fsharp-cheatsheet.md"), template,
         Path.Combine(output, "fsharp-cheatsheet.tex"), OutputKind.Latex,
@@ -17,4 +28,5 @@ let generateLatexDoc() =
 
 #time "on";;
 
-generateLatexDoc()
+generateHtmlDoc();;
+generateLatexDoc();;
