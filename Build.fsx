3af042dffae63ae8fd0fc07379a8a3700477a7e9
Phan Anh Dung <phananhdung309@yahoo.com>
2013-06-03 19:08:09 +0200

    Add Latex template and project structure

---
+++ Build.fsx
@@ -0,0 +1,20 @@
+﻿#I "packages/FSharp.Formatting.1.0.15/lib/net40"
+#load "packages/FSharp.Formatting.1.0.15/literate/literate.fsx"
+
+open System.IO
+open FSharp.Literate
+
+let generateLatexDoc() =
+  let source = __SOURCE_DIRECTORY__
+  let template = Path.Combine(source, "./templates/template-cheatsheet.tex")
+  let sources = Path.Combine(source, ".")
+  let output = Path.Combine(source, ".")
+
+  Literate.ProcessMarkdown
+      ( Path.Combine(source, "fsharp-cheatsheet.md"), template,
+        Path.Combine(output, "fsharp-cheatsheet.tex"), OutputKind.Latex,
+        includeSource = true)
+
+#time "on";;
+
+generateLatexDoc()
---
+++ fsharp-cheatsheet.md
@@ -0,0 +1,20 @@
+F# 3.0 cheatsheet
+---------------
+Contents follow here.
+
+
+Comments
+---------------
+Contents follow here.
+
+
+Strings
+-----------
+
+Contents follow here.
+
+
+Numbers
+------------------------
+
+Contents follow here.
